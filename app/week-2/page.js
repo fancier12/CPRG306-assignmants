243e16a18adaa0759e9eadf70c3daaa1baeb7152
fancier12 <bidhi.poudyal@edu.sait.ca>
2024-05-27 10:28:17 -0600

    week-2

---
+++ app/week-2/page.js
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+      <main>
+        <h1>week 2</h1>
+      </main>
+    );
+  }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,14 @@
+/ week-2/student-info.js
+
+import Link from 'next/link';
+
+const StudentInfo = () => {
+  return (
+    <div>
+      <h1>Bidhi Poudyal</h1>
+      <Link href="">
+        <a target="_blank">My GitHub Repository</a>
+      </Link>
+    </div>
+  );
+};
